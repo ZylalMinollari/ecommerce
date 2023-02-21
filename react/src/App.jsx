f4090c34a5f96e820654ded4c0aca27026d8415f
Glejd <glejd.qamirani@landmark.al>
2023-02-21 15:46:49 +0100

    commit test

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     const [count, setCount] = useState(0);
 
     return <div className="App">
-        <h1>Hello Medi Tahremi</h1>
+        <h1>Hello Medi King Tahremi</h1>
     </div>;
 }
 
